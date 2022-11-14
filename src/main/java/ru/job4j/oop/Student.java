05481ec2b3dd04f035294d739dcc9f27da471bd9
Kannayonas <Alinochka.nsk@ya.ru>
2022-11-14 23:17:44 +0700

    1.2. Вызов метода объекта [#166085]

--- src/main/java/ru/job4j/oop/Student.java
+++ src/main/java/ru/job4j/oop/Student.java
@@ -1,10 +1,10 @@
 package ru.job4j.oop;
 
 public class Student {
-
     public void music() {
         System.out.println("Tra tra tra");
     }
+
     public void  song() {
         System.out.println("I believe I can fly");
     }
